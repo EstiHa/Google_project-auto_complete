7e85ec707c4ff7c548c595d9aaa1e6221d66ad33
EstiHa <st3212445@gmail.com>
2021-07-14 13:39:35 +0300

    14/07/2021 13:39

---
+++ sentence.py
@@ -0,0 +1,10 @@
+class Sentence:
+    def __init__(self, sentence, location):
+        self.sentence=sentence
+        self.location=location
+
+    def get_sentence(self):
+        return self.sentence
+
+    def get_location(self):
+        return self.location
