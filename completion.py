70d744c4ef4705edfcba389accc3d75d4089ea1d
EstiHa <st3212445@gmail.com>
2021-07-15 11:52:09 +0300

    15/07/2021 11:51

--- completion.py
+++ completion.py
@@ -1,22 +1,23 @@
 from trie import Trie
 from sentences_collection import SentencesCollection
 
+
 class Completion:
     def __init__(self):
-        self.trie=Trie.getInstance()
-        self.sentences_collection=SentencesCollection()
-        self.prev_sentences=None
+        self.trie = Trie.getInstance()
+        self.sentences_collection = SentencesCollection()
+        self.prev_sentences = None
 
     def search_completions(self, text):
-        if text=='#':
-            self.prev_sentences=True
+        if text == '#':
+            self.prev_sentences = None
             return
-        sentences_id=self.trie.search(text)
+        sentences_id = self.trie.search(text)
         if self.prev_sentences is None:
-            sentences=self.get_five_sentences(sentences_id)
+            sentences = self.get_five_sentences(sentences_id)
         else:
-            relevant_sentences_id=self.get_relevant_sentences(sentences_id)
-            sentences=self.get_five_sentences(sentences_id)
+            sentences_id = self.get_relevant_sentences(sentences_id)
+            sentences = self.get_five_sentences(sentences_id)
         # completed_sentences=[]
         # for i in range(5):
         #     sen_obj=self.sentences_collection.get_sentence_obj(str(sentences_id[i]))
@@ -26,12 +27,13 @@ class Completion:
         # res=[]
         # [res.append(x) for x in completed_sentences if x not in res]
         # return res
+        self.prev_sentences = sentences_id
         return sentences
 
-    #Ewxtracr=t five objects of sentences by id from the fit sentences id list.
+    # Extract five objects of sentences by id from the fit sentences id list.
     def get_five_sentences(self, sentences_id):
-        if len(sentences_id)==0:
-            return None
+        if len(sentences_id) == 0:
+            return "not found"
         completed_sentences = []
         for i in range(5):
             sen_obj = self.sentences_collection.get_sentence_obj(str(sentences_id[i]))
@@ -42,6 +44,7 @@ class Completion:
         [res.append(x) for x in completed_sentences if x not in res]
         return res
 
-    #Find the intersaction of the previous fit sentences and the current.
+    # Find the intersection of the previous fit sentences and the current.
     def get_relevant_sentences(self, sentences_id):
-        pass
+        sen_ids = [id for id in sentences_id if id in self.prev_sentences]
+        return sen_ids
--- sentence.py
+++ sentence.py
@@ -1,10 +1,15 @@
 class Sentence:
-    def __init__(self, sentence, location):
+    def __init__(self, sentence, location, index):
         self.sentence=sentence
         self.location=location
+        self.sentence_index=index
+
 
     def get_sentence(self):
         return self.sentence
 
     def get_location(self):
         return self.location
+
+    def get_sentence_index(self):
+        return self.sentence_index
